dc5af0fe3bdfea8f3e63027c6c6481e3a07cba6d
Kevin Reinholz <kreinholz@gmail.com>
2024-10-17 16:14:41 -0700

    Modified patch-cmake_Modules_FindFFmpeg.cmake to more aggressively point to my custom ffmpeg3-3.0.2 port's install location. In theory this should allow 'dirty' builds to proceed without first uninstalling ffmpeg-6.x, although I haven't tested it aside from a clean rebuild using synth.

--- files/patch-cmake_Modules_FindFFmpeg.cmake
+++ files/patch-cmake_Modules_FindFFmpeg.cmake
@@ -1,16 +1,40 @@
---- cmake/Modules/FindFFmpeg.cmake.orig	2024-05-21 19:41:04 UTC
+--- cmake/Modules/FindFFmpeg.cmake.orig	2024-02-04 13:08:02 UTC
 +++ cmake/Modules/FindFFmpeg.cmake
-@@ -80,7 +80,7 @@ function(find_ffmpeg LIBNAME)
+@@ -62,33 +62,27 @@ function(find_ffmpeg LIBNAME)
+ 
+   if(FFMPEG_DIR)
+     list(APPEND INCLUDE_PATHS
+-      ${FFMPEG_DIR}
+-      ${FFMPEG_DIR}/ffmpeg
+-      ${FFMPEG_DIR}/lib${LIBNAME}
+-      ${FFMPEG_DIR}/include/lib${LIBNAME}
+-      ${FFMPEG_DIR}/include/ffmpeg
+-      ${FFMPEG_DIR}/include
++      /usr/local/ffmpeg3/
++      /usr/local/ffmpeg3/include/lib${LIBNAME}
++      /usr/local/ffmpeg3/include
+       NO_DEFAULT_PATH
+       NO_CMAKE_FIND_ROOT_PATH
+     )
+     list(APPEND LIB_PATHS
+-      ${FFMPEG_DIR}
+-      ${FFMPEG_DIR}/lib
+-      ${FFMPEG_DIR}/lib${LIBNAME}
++      /usr/local/ffmpeg3/
++      /usr/local/ffmpeg3/lib
++      /usr/local/ffmpeg3/lib${LIBNAME}
+       NO_DEFAULT_PATH
+       NO_CMAKE_FIND_ROOT_PATH
      )
    else()
      list(APPEND INCLUDE_PATHS
 -      /usr/local/include/ffmpeg
-+      /usr/local/ffmpeg3/include
 -      /usr/local/include/lib${LIBNAME}
+-      /usr/include/ffmpeg
+-      /usr/include/lib${LIBNAME}
+-      /usr/include/ffmpeg/lib${LIBNAME}
++      /usr/local/ffmpeg3/include
 +      /usr/local/ffmpeg3/include/lib${LIBNAME}
-       /usr/include/ffmpeg
-       /usr/include/lib${LIBNAME}
-@@ -88,7 +88,7 @@ function(find_ffmpeg LIBNAME)
      )
  
      list(APPEND LIB_PATHS
